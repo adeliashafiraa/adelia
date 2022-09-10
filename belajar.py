1b81ac8c7a07acccd44238e7f3ec7237191d7806
adeliashafiraa <113223407+adeliashafiraa@users.noreply.github.com>
2022-09-10 10:33:41 +0700

    latihan coding himasi

---
+++ belajar.py
@@ -0,0 +1,34 @@
+def fungsi_python():
+    print ("hai ini adalah python")
+    
+
+nama = "Adelia Shafira"
+umur = 17
+print("Nama saya adalah  "+ nama + " Umur saya adalah " + str(umur))
+print("Umur saya adalah : ", umur)
+
+
+# List <Array>
+hewan = ["Ayam", "Kucing", "Aligator", "katak"]
+print(hewan)
+
+
+
+# fungsi_python()
+# INI KOMENTAR
+# INI BUKAN KOMENTAR
+# CTRL + /
+
+
+angka1 = 4
+angka2 = 9
+angka3 = 24
+print ("Penjumlahan : " + str(angka1 + angka2 + angka3 + 24))
+
+print ("Pengurangan : " + str(angka1 - angka2 - angka3 - 24))
+
+print ("Pembagian : " + str(angka1 / angka2 / angka3 / 24))
+
+print ("Pembagian 2 : " + str(angka1 // angka2))
+
+
